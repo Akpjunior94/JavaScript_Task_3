ae78b95f784101efc104eb2f1a5b4b2797acebb6
Akpan Abraham <Akpjunio4real@gmail.com>
2020-04-19 15:47:16 -0700

    jS task 3 added

---
+++ script.js
@@ -0,0 +1,33 @@
+function yoGiOh(number) {
+  let array = [];
+  let output;
+
+  for (let i = 1; i <= number; i++) {
+    
+      if (i % 2 === 0 && i % 3 ===0 && i % 5 === 0) {
+        output = "yo-gi-oh";
+      } else if (i % 2 === 0 && i % 3 === 0) {
+        output = "yo-g";
+      } else if (i % 2 === 0 && i % 5 === 0) {
+        output = "yo-oh";
+      } else if (i % 3 === 0 && i % 5 === 0) {
+        output = "gi-oh";
+      } else if (i % 2 === 0 ) {
+        output = "yo";
+      } else if ( i % 3 === 0 ) {
+        output = "gi";
+      } else if (i % 5 === 0 ) {
+        output = "oh";
+      } else {
+        output = i;
+      }
+
+      array.push(output);
+  };
+
+  console.log(array);
+  return array;    
+};
+
+yoGiOh(100);
+yoGiOh(54)
