ac1e86a38811e2d90df74a4ac3a5bf6b4037633a
Akpan Abraham <56775903+Akpjunior94@users.noreply.github.com>
2020-04-20 06:58:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function yoGiOh(number) {
+function yuGiOh(number) {
   let array = [];
   let output;
 
   for (let i = 1; i <= number; i++) {
     
       if (i % 2 === 0 && i % 3 ===0 && i % 5 === 0) {
-        output = "yo-gi-oh";
+        output = "yu-gi-oh";
       } else if (i % 2 === 0 && i % 3 === 0) {
-        output = "yo-g";
+        output = "yu-gi";
       } else if (i % 2 === 0 && i % 5 === 0) {
-        output = "yo-oh";
+        output = "yu-oh";
       } else if (i % 3 === 0 && i % 5 === 0) {
         output = "gi-oh";
       } else if (i % 2 === 0 ) {
